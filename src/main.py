6ef271404ce1b90b9c0f32ff322dc4b9be6b735a
Christian Schoeberl <christianschoeberl@ChristiansMBP2.attlocal.net>
2020-12-15 16:54:55 -0600

    directions and funcs

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ import json
 import requests
 from geopy import distance
 from geopy.geocoders import Nominatim
+import requests
+from bs4 import BeautifulSoup
 
 
 def welcome():
@@ -34,7 +36,9 @@ def Weather():
 #Get user input- current and destination addresses
 def getAddresses():
     CurrentAddress = input("Enter current address (number, street name): ")
-    atLocation = input("Is your current address same as destination (Y/N):")
+    atLocation = input("Is your current address same as destination " + "\n" +
+                       "(Y if looking for racks closest to you) " + 
+                       "(Y/N): ")
     if atLocation == 'Y':
         destAddress = CurrentAddress
     else:
@@ -43,7 +47,8 @@ def getAddresses():
 
 # Get user input- type of coverage
 def coverageType():
-    choice = input('Enter where do you want to park your bike\n Outdoor, Indoor, or No Preference:')
+    choice = input('Enter where do you want to park your bike\n' +
+                   "Outdoor, Indoor, or No Preference: ")
     if choice == 'Outdoor':
         choice = 'Outdoor'
     elif choice == 'Indoor':
@@ -69,6 +74,8 @@ def rackLoc(destCoor,choice):
         dist = distance.distance(destCoor, point).feet
         Distance.append(dist)
     racks_df['Distance'] = Distance
+    racks_df = racks_df.sort_values(by=['Distance'])
+    racks_df = racks_df.reset_index(drop=True)
     if choice == 'NP':
         return(racks_df[racks_df['Distance'] <= 1320])
     else: 
@@ -122,14 +129,42 @@ def propGroups(locations, min, max, third):
         prop = locations.iloc[i][3]
         if prop <= (min + third):
             groups.append('Low')
-            print("here")
         if prop > (min + third) and prop <= (max - third):
             groups.append('Medium')
         if prop > (max - third):
             groups.append('High')
     return groups
 
-locations = findLocations()
+def choiceSelect(destRacks):
+    numChoices = len(destRacks)
+    pref = int(input("Please select bike rack choice (0-" + str(numChoices)
+                     + "): "))
+    return pref
+
+#initializing locations from WPRDC csv file
+#return: list of locations of all bike racks 
+def findLocationsc():
+    bike_locations = pd.read_csv("bike_rack_location.csv")
+    bike_locations_loc = bike_locations[['Longitude', 'Latitude']]
+    return bike_locations_loc
+
+#finding directions between input start and end coordinates 
+#params: long_start, lat_start: starting coordinates; 0 index column in findLocations() is long
+#params: long_end, lat_end: ending coordiantes
+#return: list of directions 
+def getDirections(long_start, lat_start, long_end, lat_end):
+    page = requests.get("https://www.mapquest.com/directions/list/1/near-" + str(lat_start) + ","
+                        + str(long_start) + "/to/near-" + str(lat_end) + "," + str(long_end))
+    page.raise_for_status()
+    soup = BeautifulSoup(page.content, 'html.parser')
+    narrative = soup.find(id="primaryPanel")
+    directions = narrative.find("ol").getText()
+    directions_format = directions.split("\n")
+    directions_format = [i for i in directions_format if i]
+    return directions_format 
+
+print("<please wait ~1min while we load>")
+locations = findLocationsc()
 locations_thefts = locations.copy(deep=True)
 thefts = allTheft(locations)
 locations_thefts["Thefts"] = thefts
@@ -143,6 +178,47 @@ interval = prop_max - prop_min
 interval_third = interval/3 
 prop_groups = propGroups(locations_thefts, prop_min, prop_max, interval_third)
 locations_thefts["Group"] = prop_groups 
-print(locations_thefts[["Latitude","Longitude","Group"]])
+locations_thefts = locations_thefts[["Latitude","Longitude","Group"]]
+Weather()
+welcome()
+addresses = getAddresses()
+startCoords = 0
+endCoords = 0
+long_start= 0
+lat_start= 0
+long_end= 0
+lat_start = 0
+counter = 1
+for i in addresses:
+    if counter == 1:
+        startCoords = addtoLoc(i)
+        counter+=1
+        long_start = startCoords[0]
+        lat_start = startCoords[1]
+    if counter == 2:
+        endCoords = addtoLoc(i)
+        long_end = endCoords[0]
+        lat_end = endCoords[1]
+coveragePref = coverageType()
+destRacks = rackLoc(endCoords, coveragePref)
+groupAssign = []
+for i in range(len(destRacks)):
+    longCheck = destRacks.iloc[i][0]
+    latCheck = destRacks.iloc[i][1]
+    for s in range(len(locations_thefts)):
+        theftLong = locations_thefts.iloc[s][1]
+        theftLat = locations_thefts.iloc[s][0]
+        if longCheck == theftLong and latCheck == theftLat:
+            groupAssign.append(locations_thefts.iloc[s][2])
+destRacks["Group"] = groupAssign
+print(destRacks)
+pref = choiceSelect(destRacks)
+for i in range(len(destRacks)):
+    if i == pref:
+        long_end = destRacks.iloc[i][0]
+        lat_end = destRacks.iloc[i][1]
+directions_format = getDirections(long_start, lat_start, long_end, lat_end)
+for i in directions_format:
+    print(i)
 
 
